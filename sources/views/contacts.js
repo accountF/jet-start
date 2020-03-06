1c78dbf6c5bb1f10ac09a3cfc96821a9196eda5b
accountF <kittylora.33@gmail.com>
2020-03-06 15:57:10 +0300

    added directory for contacts

--- sources/views/contacts.js
+++ sources/views/contacts.js
@@ -1,6 +1,6 @@
 import {JetView} from "webix-jet";
-import Form from "views/formInContacts.js";
-import List from "views/listInContacts.js";
+import Form from "views/contacts/formInContacts.js";
+import List from "views/contacts/listInContacts.js";
 
 export default class ContactsView extends JetView {
 	config() {
