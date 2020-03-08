4a110cc292cd7ddfc970d1cdf6b28b1982917061
accountF <kittylora.33@gmail.com>
2020-03-09 02:21:57 +0300

    lesson 2 done

--- sources/views/contacts/formInContacts.js
+++ sources/views/contacts/formInContacts.js
@@ -1,21 +1,43 @@
 import {JetView} from "webix-jet";
+import { contacts } from "models/contacts.js";
 
 export default class FormInContactsView extends JetView {
 	config() {
+		const _ = this.app.getService("locale")._;
 		return {
 			view: "form",
+			localId: "myForm",
 			autoheight: false,
 			elements: [
-				{view: "text", label: "Name"},
-				{view: "text", label: "E-mail"},
+				{view: "text", label: _("Name"), name: "Name"},
+				{view: "text", label: _("E-mail"), name: "Email"},
+				{view: "combo", label: _("Countries"), name: "Country", value:"",
+					options:[{value:1}, {value:2}, {value:3}]},
+				{view: "combo", label: _("Statuses"), name: "Status", value:"",
+					options:[{value:1}, {value:2}, {value:3}]},
 				{
 					cols: [
-						{view: "button", value: "Add"},
-						{view: "button", value: "Clear"}
+						{view: "button", value: _("Save"), click: () => this.saveContact()}
 					]
 				}
-
 			]
 		};
+	}
+
+	init(){
+		this.form = this.$$("myForm");
+		this.on(this.app, "onChangeUrlInList", (item) => {
+			this.form.setValues(item);
+		});
+	}
+
+	saveContact(){
+		const dataFromForm = this.form.getValues();
+		if(dataFromForm.id){
+			contacts.updateItem(dataFromForm.id, dataFromForm);
+		} else {
+			contacts.add(dataFromForm);
+		}
+		this.form.clear();
 	}
 }
--- sources/views/contacts/listInContacts.js
+++ sources/views/contacts/listInContacts.js
@@ -3,14 +3,58 @@ import {contacts} from "models/contacts.js";
 
 export default class ListInContactsView extends JetView {
 	config() {
+		const _ = this.app.getService("locale")._;
 		return {
-			view: "list",
-			select: true,
-			template: "#Name#, e-mail: #Email#"
+			rows: [
+				{
+					view: "list",
+					localId: "contactsList",
+					select: true,
+					template: "#Name#, e-mail: #Email# <span class='webix_icon wxi-close'></span>",
+					onClick: {
+						"wxi-close": (e, id) => this.deleteItem(id)
+					}
+				},
+				{
+					view: "button",
+					value: _("Add"),
+					click: () => this.addItemIntoList()
+				}
+			]
+
 		};
 	}
 
-	init(view) {
-		view.parse(contacts);
+	init(view, url) {
+		this.list = this.$$("contactsList");
+		this.list.sync(contacts);
+		this.list.attachEvent("onAfterSelect", (id) => this.setIdIntoUrl(id));
+
+		if (url[0].params.id) {
+			this.list.select(url[0].params.id);
+		} else {
+			this.list.select(this.list.getFirstId());
+		}
+	}
+
+	addItemIntoList(){
+		const id = contacts.add({Name:"Name", Email:"lalala@mail.ru"});
+		this.list.select(id);
+		this.setIdIntoUrl(id);
+	}
+
+	deleteItem(id){
+		contacts.remove(id);
+		this.setIdIntoUrl(id);
+	}
+
+	setIdIntoUrl(id) {
+		this.setParam("id", id, true);
+	}
+
+	urlChange() {
+		const id = this.getParam("id");
+		const itemById = this.list.getItem(id);
+		this.app.callEvent("onChangeUrlInList", [itemById]);
 	}
 }
